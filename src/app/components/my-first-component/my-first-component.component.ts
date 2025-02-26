3a94a564ce370424b88e199d52d56bd66dde27bf
Volodymyr Maliarov <v.maliarov1996@gmail.com>
2025-02-26 16:49:48 +0100

    lesson 2

--- src/app/components/my-first-component/my-first-component.component.ts
+++ src/app/components/my-first-component/my-first-component.component.ts
@@ -1,6 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { max } from 'rxjs';
 
+export interface testObjectInterface {
+  name: string;
+  sername: string;
+  age: number;
+  hasWork: boolean;
+}
+
 @Component({
   selector: 'app-my-first-component',
   templateUrl: './my-first-component.component.html',
@@ -8,13 +15,72 @@ import { max } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
+
+// number -> 1,323435,435345,
+// string -> 'rjghergjkehgrjkerh'
+// boolean -> true / false
 export class MyFirstComponentComponent implements OnInit {
   public counter = 0;
 
-  // pu
+  public name!: string;
+  public sername!: string;
+  public age!: number;
+  public hasWork!: boolean;
+
+  public myFirstArray: number[] = [1, 3, 5, 100, 7, 8, 8, 9];
+
+  public testObject: testObjectInterface = {
+    name: 'Vova',
+    sername: 'Mal',
+    age: 12,
+    hasWork: true,
+  };
+
+  public userList: testObjectInterface[] = [
+    {
+      name: 'Vova',
+      sername: 'Mal',
+      age: 12,
+      hasWork: true,
+    },
+    {
+      name: 'Sasha',
+      sername: 'Mal',
+      age: 19,
+      hasWork: false,
+    },
+    {
+      name: 'David',
+      sername: 'Mal',
+      age: 22,
+      hasWork: true,
+    },
+    {
+      name: 'Vova',
+      sername: 'Mal',
+      age: 12,
+      hasWork: true,
+    },
+    {
+      name: 'Sasha',
+      sername: 'Mal',
+      age: 19,
+      hasWork: false,
+    },
+    {
+      name: 'David',
+      sername: 'Mal',
+      age: 22,
+      hasWork: true,
+    },
+  ];
 
   ngOnInit(): void {
     this.forArrayExample();
+  }
+
+  addNewName(nameFromOur: string): void {
+    this.name = nameFromOur;
   }
 
   arrayFun(): void {
@@ -22,15 +88,23 @@ export class MyFirstComponentComponent implements OnInit {
   }
 
   forArrayExample() {
-    // this.myFirstArray.forEach((el) => {
-    //   console.log('every elemnt', el);
-    // });
-    // console.log('reverse', this.myFirstArray.reverse());
-    // console.log(
-    //   'find',
-    //   this.myFirstArray.find((el) => el === 8)
-    // );
-    // console.log('max', Math.max(...this.myFirstArray));
+    let maxValue = 0;
+    this.myFirstArray.forEach((el) => {
+      if (el > maxValue) {
+        maxValue = el;
+      }
+    });
+
+    console.log('max value', maxValue);
+
+    console.log('max', Math.max(...this.myFirstArray));
+
+    console.log('reverse', this.myFirstArray.reverse());
+    console.log(
+      'find',
+      this.myFirstArray.find((el) => el === 8)
+    );
+    console.log('max', Math.max(...this.myFirstArray));
   }
 
   showConsoleLog(text: string): void {
