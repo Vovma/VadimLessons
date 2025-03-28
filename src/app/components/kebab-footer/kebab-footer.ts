5943786559ca9c0bf30b0fad0b7c938aba8b7ed0
Volodymyr Maliarov <v.maliarov1996@gmail.com>
2025-03-28 13:40:46 +0100

    VADIM-Last lesson 28.03

--- src/app/components/kebab-footer/kebab-footer.ts
+++ src/app/components/kebab-footer/kebab-footer.ts
@@ -7,4 +7,54 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
-export class KebabFooter {}
+export class KebabFooter {
+  public kebabList = [
+    {
+      name: 'Kebab1',
+      price: '27 PLN',
+      meet: 'Chicken',
+      weight: '120g',
+      sous: 'Ketchup',
+    },
+    {
+      name: 'Kebab2',
+      price: '30 PLN',
+      meet: 'Chicken',
+      weight: '120g',
+      sous: 'Ketchup',
+    },
+    {
+      name: 'Kebab3',
+      price: '38 PLN',
+      meet: 'Korowa',
+      weight: '120g',
+      sous: 'Mayonez',
+    },
+  ];
+
+  public homeWorkKebabElement = {
+    name: 'Kebab1',
+    price: '27 PLN',
+    meet: 'chicken',
+    weight: '120g',
+    sous: 'ketchup',
+  };
+
+  // nameWhatYouWant -> Kebab elemnt
+  // example
+  // {
+  //   name: 'Kebab3',
+  //   price: '38 PLN',
+  //   meet: 'Korowa',
+  //   weight: '120g',
+  //   sous: 'Mayonez',
+  // },
+  openProduct(nameWhatYouWant: any): void {
+    console.log('open product', nameWhatYouWant);
+    this.homeWorkKebabElement = nameWhatYouWant;
+  }
+
+  addToCard(nameWhatYouWant: any): void {
+    console.log('add to card', nameWhatYouWant);
+  }
+}
