18d467806457e8b192ee341467a67053e2d7e6c8
Vadym-S-s <vam.kanon@gmail.com>
2025-04-22 20:18:24 +0200

    Vadim homework form/scss

--- src/app/components/kebab-creation-form/kebab-creation-form.component.ts
+++ src/app/components/kebab-creation-form/kebab-creation-form.component.ts
@@ -67,6 +67,81 @@ export class KebabCreationFormComponent {
   ];
 
   public currencyList = ['PLN', 'USD', 'EUR'];
+  public sauseList = [
+    'Garlic sauce',
+    'Spicy chili sauce',
+    'Yogurt-mint sauce',
+    'Tzatziki',
+    'BBQ sauce',
+    'Sweet and sour sauce',
+    'Curry sauce',
+    'Tahini sauce',
+    'White sauce',
+    'Hot sriracha mayo',
+    'Cheese sauce',
+    'Hummus',
+    'Mustard sauce',
+    'Tomato ketchup',
+    'Smoky paprika sauce',
+  ];
+  public ratingList = [
+    '⭐ 1 - Very poor',
+    '⭐⭐ 2 - Poor',
+    '⭐⭐⭐ 3 - Average',
+    '⭐⭐⭐⭐ 4 - Good',
+    '⭐⭐⭐⭐⭐ 5 - Excellent',
+  ];
+
+  public nutritionList = [
+    'High Protein',
+    'Low Fat',
+    'Low Carb',
+    'Rich in Fiber',
+    'Gluten Free',
+    'Lactose Free',
+    'Vegan Friendly',
+    'Vegetarian',
+    'Keto Friendly',
+    'Contains Omega-3',
+    'Rich in Vitamins',
+    'High Calorie',
+    'Low Calorie',
+  ];
+  public spicyLevelList = [
+    'Not Spicy',
+    'Mild',
+    'Medium',
+    'Spicy🌶️',
+    'Extra Spicy🌶️🌶️',
+    'Nuclear 🌶️🌶️🌶️',
+  ];
+  public brandList = [
+    'BerlinKebab',
+    'Kebab Master',
+    'Grill & Wrap',
+    'Kebab Bros',
+    'Doner King',
+    'Urban Kebab Co.',
+  ];
+  public vegetarianList = [
+    'Yes',
+    'No',
+    'Vegan',
+    'Vegetarian with dairy',
+    'Vegetarian with eggs',
+  ];
+  public caloriesList = [
+    'Less than 300 kcal',
+    '300–500 kcal',
+    '500–700 kcal',
+    '700–900 kcal',
+    'More than 900 kcal',
+  ];
+  public fatList = [
+    'Low fat (under 5g)',
+    'Medium fat (5g–15g)',
+    'High fat (over 15g)',
+  ];
 
   createKebabForm = new FormGroup({
     id: new FormControl(''),
